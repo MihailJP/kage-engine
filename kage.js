3dae075034e8f898b5653ad3ef40b90d7b1a087c
MihailJP <mihailjp@gmail.com>
2016-03-19 12:41:54 +0900

    Add Socho font engine

--- kage.js
+++ kage.js
@@ -8,7 +8,7 @@ function Kage(size){
   
   function makeGlyph2(polygons, data){ // void
       if(data != ""){
-	  var strokesArray = this.adjustKirikuchi(this.adjustUroko2(this.adjustUroko(this.adjustKakato(this.adjustTate(this.adjustMage(this.adjustHane(this.getEachStrokes(data))))))));
+	  var strokesArray = this.adjustRoof(this.adjustLeftTop(this.adjustKirikuchi(this.adjustUroko2(this.adjustUroko(this.adjustKakato(this.adjustTate(this.adjustMage(this.adjustHane(this.getEachStrokes(data))))))))));
 	  for(var i = 0; i < strokesArray.length; i++){
 	      dfDrawFont(this, polygons,
 			 strokesArray[i][0],
@@ -30,7 +30,7 @@ function Kage(size){
   function makeGlyph3(data){ // void
       var result = new Array();
       if(data != ""){
-	  var strokesArray = this.adjustKirikuchi(this.adjustUroko2(this.adjustUroko(this.adjustKakato(this.adjustTate(this.adjustMage(this.adjustHane(this.getEachStrokes(data))))))));
+	  var strokesArray = this.adjustRoof(this.adjustLeftTop(this.adjustKirikuchi(this.adjustUroko2(this.adjustUroko(this.adjustKakato(this.adjustTate(this.adjustMage(this.adjustHane(this.getEachStrokes(data))))))))));
 	  for(var i = 0; i < strokesArray.length; i++){
 	      var polygons = new Polygons();
 	      dfDrawFont(this, polygons,
@@ -307,6 +307,72 @@ function Kage(size){
   }
   Kage.prototype.adjustKakato = adjustKakato;
   
+  function adjustLeftTop(strokesArray){ // strokesArray
+    if (this.kShotai == this.kSocho) {
+      var R2LDlines = new Array();
+      for(var i = 0; i < strokesArray.length; i++){
+        if ((strokesArray[i][0] == 2) && (strokesArray[i][2] % 100 == 7)) {
+          R2LDlines.push(
+            [strokesArray[i][3], strokesArray[i][4], strokesArray[i][5], strokesArray[i][6]],
+            [strokesArray[i][5], strokesArray[i][6], strokesArray[i][7] + this.kMinWidthT / 2, strokesArray[i][8]]);
+        }
+      }
+      for(var i = 0; i < strokesArray.length; i++){
+        if ((strokesArray[i][0] == 1) && (strokesArray[i][3] == strokesArray[i][5])) {
+          for (var R2LDindex = 0; R2LDindex < R2LDlines.length; R2LDindex++) {
+            var xx0 = strokesArray[i][3];
+            var yy0 = strokesArray[i][4];
+            var xx1 = R2LDlines[R2LDindex][0];
+            var yy1 = R2LDlines[R2LDindex][1];
+            var xx2 = R2LDlines[R2LDindex][2];
+            var yy2 = R2LDlines[R2LDindex][3];
+            if ((xx2 <= xx0) && (xx0 <= xx1) && (xx1 != xx2)) {
+              var slope = (yy2 - yy1) / (xx2 - xx1);
+              var yIntercept = yy1 - slope * xx1;
+              if ((yy0 < (xx0 * slope + yIntercept + this.kMinWidthY * 4)) && (yy0 > (xx0 * slope + yIntercept - this.kMinWidthY * 2))) {
+                strokesArray[i][1] += 100000; break;
+              }
+            }
+          }
+        }
+      }
+    }
+    return strokesArray;
+  }
+  Kage.prototype.adjustLeftTop = adjustLeftTop;
+
+  function adjustRoof(strokesArray){ // strokesArray
+    if (this.kShotai == this.kSocho) {
+      var horizLines = new Array();
+      for(var i = 0; i < strokesArray.length; i++){
+        if ((strokesArray[i][0] == 1) && (strokesArray[i][4] == strokesArray[i][6])) {
+          horizLines.push(
+            [strokesArray[i][3], strokesArray[i][4], strokesArray[i][5], strokesArray[i][6]]);
+        }
+      }
+      for(var i = 0; i < strokesArray.length; i++){
+        if ((strokesArray[i][0] == 2) && (strokesArray[i][1] == 0) && (strokesArray[i][2] == 7)) {
+          for (var lineIndex = 0; lineIndex < horizLines.length; lineIndex++) {
+            var xx0 = strokesArray[i][3];
+            var yy0 = strokesArray[i][4];
+            var xx1 = horizLines[lineIndex][0];
+            var yy1 = horizLines[lineIndex][1];
+            var xx2 = horizLines[lineIndex][2];
+            var yy2 = horizLines[lineIndex][3];
+            if ((xx1 <= xx0) && (xx0 <= xx2)) {
+              if ((yy0 < (yy1 + this.kMinWidthY * 4)) && (yy0 > (yy1 - this.kMinWidthY * 2))) {
+                strokesArray[i][1] += 1000000; break;
+              }
+            }
+          }
+        }
+      }
+    }
+    return strokesArray;
+  }
+  Kage.prototype.adjustRoof = adjustRoof;
+
+
   function getBox(glyph){ // minX, minY, maxX, maxY
       var a = new Object();
       a.minX = 200;
@@ -363,6 +429,7 @@ function Kage(size){
   //properties
   Kage.prototype.kMincho = 0;
   Kage.prototype.kGothic = 1;
+  Kage.prototype.kSocho = 2;
   this.kShotai = this.kMincho;
   
   this.kRate = 100;
